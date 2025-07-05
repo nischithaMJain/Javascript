5ac0bb84319eca7039ccbaa6fa9f08ca684d628e
Nischitha M <nmjnishchitha@gmail.com>
2025-07-05 20:05:48 +0530

    bug(JSD05): negative not a string edge cases covered

--- 01 – Core & Basics (Day 1–14)/JSD5-FizzBuzz.js
+++ 01 – Core & Basics (Day 1–14)/JSD5-FizzBuzz.js
@@ -1,4 +1,13 @@
 function fizzBuzz(n) {
+    if (typeof n !== "number") {
+        alert('Not a number')
+    }
+    if (n < 0) {
+        alert('Not an integer')
+    }
+    if (!Number.isInteger) {
+        alert('Accepts only Integer')
+    }
     for (let i = 0; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
