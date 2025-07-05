8b9cb85f3998182641b2c86dc4d5bb4e742f35af
Nischitha M <nmjnishchitha@gmail.com>
2025-07-05 09:10:37 +0530

    fix(JSD03-D3-convert-a-number

---
+++ 01 – Core & Basics (Day 1–14)/JSD3-conversionStrNo.js
@@ -0,0 +1,12 @@
+//number to string
+let a = 3;
+console.log(3 + '3');
+console.log(a.toString());
+console.log(String(a));
+console.log(a + '')
+
+
+
+//string to number
+let b = "3"
+console.log(typeof +b);
