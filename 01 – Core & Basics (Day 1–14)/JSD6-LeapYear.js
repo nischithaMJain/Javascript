ee7701c600d05ec2fcc8d166b7761c35f8845715
Nischitha M <nmjnishchitha@gmail.com>
2025-07-06 13:44:20 +0530

    feat(JSD06):leap year

---
+++ 01 – Core & Basics (Day 1–14)/JSD6-LeapYear.js
@@ -0,0 +1,12 @@
+function checkLeapYear(year) {
+    if (typeof year !== "number") return "Acceps only number"
+    if (year % 4 === 0) {
+        if (year % 400 === 0) {
+            if (year % 100 === 0) {
+                return 'IT is a leap year'
+            }
+        }
+    }
+    return 'IT is not a leap year'
+}
+checkLeapYear(2003)
