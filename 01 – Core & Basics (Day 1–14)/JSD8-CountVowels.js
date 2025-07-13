0bfa5cf3a83323309c300c09765e68d37fd4908a
Nischitha M <nmjnishchitha@gmail.com>
2025-07-13 11:12:23 +0530

    bug(TC04): uppercase validation fixed

--- 01 – Core & Basics (Day 1–14)/JSD8-CountVowels.js
+++ 01 – Core & Basics (Day 1–14)/JSD8-CountVowels.js
@@ -1,6 +1,8 @@
 function countVowels(str) {
     if (typeof str !== "string") return "Not a String!";
     let count = 0;
+    str = str.toLowerCase();
+
     for (let i = 0; i < str.length; i++) {
         if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
             count++;
