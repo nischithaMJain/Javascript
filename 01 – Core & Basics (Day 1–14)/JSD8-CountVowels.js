4ccfb8b64369e8f56c6ed5e83fcf56466cafd9d7
Nischitha M <nmjnishchitha@gmail.com>
2025-07-13 11:01:37 +0530

    fix(D11): count vowels

---
+++ 01 – Core & Basics (Day 1–14)/JSD8-CountVowels.js
@@ -0,0 +1,20 @@
+function countVowels(str) {
+    if (typeof str !== "string") return "Not a String!";
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            count++;
+        }
+    }
+    if (count === 0) {
+        return 'No Vowles found!'
+    } else {
+        return count;
+    }
+
+
+}
+
+const result = countVowels('Hello World')
+
+console.log(result)
